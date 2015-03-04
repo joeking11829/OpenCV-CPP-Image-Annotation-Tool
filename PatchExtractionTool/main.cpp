7215890cd36c09722c121af11bdc05b74c866855
Tom Runia <tomrunia@gmail.com>
2015-03-04 09:31:29 +0100

    Initial Commit

---
+++ PatchExtractionTool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PatchExtractionTool
+//
+//  Created by Tom Runia on 04/03/15.
+//  Copyright (c) 2015 TUDelft. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
